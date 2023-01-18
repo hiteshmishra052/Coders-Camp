c1040cb97998a15f76adf73c0b6327d5f0248f39
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-01-19 00:33:30 +0530

    sending data using insonia for register loin and logout

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -39,14 +39,21 @@ export const login = (req, res) => {
  
         const token = jwt.sign({id:data[0].id},"secretkey");
 
+        const { password, ...others } = data[0];
+
         res.cookie("accessToken", token, {
-            
+            httpOnly: true,
         })
+        .status(200)
+        .json(others);
 
     });
 };
 
 
 export const logout = (req, res) => {
-
-}
+    res.clearCookie("accessToken",{
+        secure:true,
+        sameSite:"none"
+      }).status(200).json("User has been logged out.")
+};
