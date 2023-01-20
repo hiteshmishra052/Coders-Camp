976d9b1dfbadf393255cb6d13beb1e5bccd7d369
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-01-20 23:52:38 +0530

    logging in and added auth

--- my-app/src/context/authContext.jsx
+++ my-app/src/context/authContext.jsx
@@ -1,21 +1,28 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export const AuthContextProvider =({children}) =>{
-    const [currentUser, setcurrentUser] = useState( JSON.parse( localStorage.getItem("user")) || null
-    );
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
-    const login = ()=>{
-        //To do
-        setcurrentUser({id:1,name:"John Doe",profilePic:"https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"})
-    };
+  const login = async (inputs) => {
+    const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+      withCredentials: true,
+    });
 
-    useEffect(()=>{
-       localStorage.setItem("user",JSON.stringify(currentUser));
-    },[currentUser]);
+    setCurrentUser(res.data)
+  };
 
-    return (
-        <AuthContext.Provider value={{currentUser, login }}>{children}</AuthContext.Provider>
-    )
-}
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- my-app/src/pages/login/login.jsx
+++ my-app/src/pages/login/login.jsx
@@ -1,40 +1,69 @@
-import React, { useContext } from 'react';
-import "./login.scss"
-import { Link } from "react-router-dom";
-import { AuthContext } from '../../context/authContext';
-const Login =  () => {
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
 
-  const {login} = useContext(AuthContext);
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
 
-  const handleLogin = ()=>{
-    login();
-  }
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/")
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
   return (
-    <div>
-      <div className="login">
-        <div className="card">
-            <div className="left">
-                <h1>Hello World</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat repellat aliquid odit totam assumenda laboriosam consectetur consequatur necessitatibus optio!
-                </p>
-                <span>Don't you have an account?</span>
-                <Link to="/register">
-                <button>Register</button>
-                </Link>
-            </div>
-            <div className="right">
-                <h1>Login</h1>
-                <form action="">
-                    <input type="text" placeholder='Username'/>
-                    <input type="password" placeholder='Passsword'/>
-                    <button onClick={handleLogin}>Login</button>
-                </form>
-            </div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleLogin}>Login</button>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
