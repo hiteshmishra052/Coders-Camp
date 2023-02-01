38e6c5aa492764c76c6bcf93648b03b795b6378f
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-02-01 23:57:16 +0530

    add posts

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -18,3 +18,20 @@ jwt.verify(token, "secretkey" , (err,userInfo)=>{
     });
 });
 };
+
+export const addPost = (req,res)=>{
+
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!")
+    
+    jwt.verify(token, "secretkey" , (err,userInfo)=>{
+        if(err) return res.status(403).json("Token is not valid!")
+        
+        const q = `INSERT INTO posts("desc","img","createdAt","userId")`;
+        
+        db.query(q,[userInfo.id, userInfo.id], (err,data)=>{
+        if(err) return res.status(500).json(err);
+        return res.status(200).json(data);
+        });
+    });
+    };
