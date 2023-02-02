39ac7d6aeb13a31a533a672ccf0a68b82a77583b
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-02-02 23:17:34 +0530

    now able to add new posts

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,6 +1,6 @@
 import {db} from "../connect.js";
 import  jwt  from "jsonwebtoken";
-
+import moment from "moment";
 
 export const getPosts = (req,res)=>{
 
@@ -27,11 +27,18 @@ export const addPost = (req,res)=>{
     jwt.verify(token, "secretkey" , (err,userInfo)=>{
         if(err) return res.status(403).json("Token is not valid!")
         
-        const q = `INSERT INTO posts("desc","img","createdAt","userId")`;
+        const q = "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+
+        const values =[
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id,
+        ];
         
-        db.query(q,[userInfo.id, userInfo.id], (err,data)=>{
+        db.query(q,[values], (err,data)=>{
         if(err) return res.status(500).json(err);
-        return res.status(200).json(data);
+        return res.status(200).json("Post has been created");
         });
     });
     };
