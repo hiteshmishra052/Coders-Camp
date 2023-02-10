a01922bb120ccc3bdcdcd88efbe72a8136d0be7b
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-02-11 00:20:10 +0530

    added the likes feature

--- my-app/src/components/post/Post.jsx
+++ my-app/src/components/post/Post.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./post.scss";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
@@ -9,13 +8,57 @@ import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
 import { useState } from "react";
 import moment from "moment";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const [commentOpen , setCommentOpen] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
-    //Temporary
-    const liked  = true;
+  const { isLoading, error, data } = useQuery(["likes", post.id], () =>
+    makeRequest.get("/likes?postId=" + post.id).then((res) => {
+      return res.data;
+    })
+  );
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (liked) => {
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", { postId: post.id });
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["likes"]);
+      },
+    }
+  );
+  const deleteMutation = useMutation(
+    (postId) => {
+      return makeRequest.delete("/posts/" + postId);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  };
+
+  const handleDelete = () => {
+    deleteMutation.mutate(post.id);
+  };
+
   return (
     <div className="post">
       <div className="container">
@@ -32,27 +75,39 @@ const [commentOpen , setCommentOpen] = useState(false);
               <span className="date">{moment(post.createdAt).fromNow()}</span>
             </div>
           </div>
-          <MoreHorizIcon />
+          <MoreHorizIcon onClick={() => setMenuOpen(!menuOpen)} />
+          {menuOpen && post.userId === currentUser.id && (
+            <button onClick={handleDelete}>delete</button>
+          )}
         </div>
         <div className="content">
-            <p>{post.desc}</p>
-            <img src={"/upload/"+post.img} alt="" />
+          <p>{post.desc}</p>
+          <img src={"/upload/" + post.img} alt="" />
         </div>
         <div className="info">
-            <div className="item">
-                {liked ?<FavoriteOutlinedIcon style={{color: "red"}}/>: <FavoriteBorderOutlinedIcon/>}
-                12 Likes
-            </div>
-            <div className="item" onClick={()=>{setCommentOpen(!commentOpen)}}>
-                <TextsmsOutlinedIcon/>
-                22 Comments
-            </div>
-            <div className="item">
-                <ShareOutlinedIcon/>
-                Share
-            </div>
+          <div className="item">
+            {isLoading ? (
+              "loading"
+            ) : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: "red" }}
+                onClick={handleLike}
+              />
+            ) : (
+              <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            )}
+            {data?.length} Likes
+          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon />
+            See Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
+          </div>
         </div>
-        {commentOpen && <Comments postId = {post.id}/>}
+        {commentOpen && <Comments postId={post.id} />}
       </div>
     </div>
   );
