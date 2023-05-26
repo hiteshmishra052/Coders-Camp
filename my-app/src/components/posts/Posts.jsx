593f24309a49bee1c63b26f6aff1c7a50fc8e8c6
hiteshmishra052 <hiteshmishra052@gmail.com>
2023-05-26 11:56:24 +0530

    fixes to profile pic and made profile page

--- my-app/src/components/posts/Posts.jsx
+++ my-app/src/components/posts/Posts.jsx
@@ -1,21 +1,14 @@
 import Post from "../post/Post";
 import "./posts.scss";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
-const Posts = () => {
-
+const Posts = ({userId}) => {
   const { isLoading, error, data } = useQuery(["posts"], () =>
-    makeRequest.get("/posts").then((res) => {
+    makeRequest.get("/posts?userId="+userId).then((res) => {
       return res.data;
     })
   );
-
-  console.log(data);
 
   return (
     <div className="posts">
